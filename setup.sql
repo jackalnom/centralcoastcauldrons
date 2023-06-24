472aed3e45eaaf3c038abb19d959ecb645bf32e4
jackalnom <jackalnom@gmail.com>
2023-06-24 11:55:12 -0700

    supabase init

--- setup.sql
+++ setup.sql
@@ -23,6 +23,88 @@ CREATE TRIGGER on_insert
 
 DROP PROCEDURE catalog_for_shop;
 
+CREATE OR REPLACE PROCEDURE purchase_from_shop(api_url text, api_key text, shop_id bigint, tick_id bigint, out error text)
+LANGUAGE plpgsql
+AS $fn$
+DECLARE
+  result http_response;
+  url text;
+  stack text;
+  message text;
+  cart_id text;
+  catalog_purchase catalog_items%ROWTYPE;
+  gold int;
+  amount_purchased int;
+BEGIN
+  SELECT * INTO catalog_purchase FROM catalog_items WHERE catalog_items.shop_id = purchase_from_shop.shop_id AND catalog_items.tick_id = purchase_from_shop.tick_id LIMIT 1;
+
+  if not found then
+    RAISE LOG 'no catalog found';
+    return;
+  end if;
+
+  BEGIN
+    PERFORM http_set_curlopt('CURLOPT_TIMEOUT_MS', '10001');
+
+    SELECT json_extract_path(content::json, 'cart_id') INTO cart_id
+      FROM http((
+          'POST',
+           api_url || 'carts/',
+           ARRAY[http_header('access_token',api_key)],
+           'application/json',
+           ''
+        )::http_request);
+  EXCEPTION WHEN OTHERS THEN
+      GET STACKED DIAGNOSTICS stack = PG_EXCEPTION_CONTEXT, message = MESSAGE_TEXT;
+      error := (message || stack);
+      return;
+  END;
+
+  BEGIN
+    PERFORM http_set_curlopt('CURLOPT_TIMEOUT_MS', '10001');
+
+    url := api_url || 'carts/' || cart_id || '/items/' || catalog_purchase.sku;
+
+    PERFORM http((
+          'PUT',
+           url,
+           ARRAY[http_header('access_token',api_key)],
+           'application/json',
+           '{"quantity": 1}'
+        )::http_request);
+  EXCEPTION WHEN OTHERS THEN
+      GET STACKED DIAGNOSTICS stack = PG_EXCEPTION_CONTEXT, message = MESSAGE_TEXT;
+      error := (message || stack);
+      return;
+  END;
+
+  BEGIN
+    PERFORM http_set_curlopt('CURLOPT_TIMEOUT_MS', '10001');
+
+    PERFORM http((
+          'POST',
+           api_url || 'carts/' || cart_id || '/checkout',
+           ARRAY[http_header('access_token',api_key)],
+           'application/json',
+           ''
+        )::http_request);
+  EXCEPTION WHEN OTHERS THEN
+      GET STACKED DIAGNOSTICS stack = PG_EXCEPTION_CONTEXT, message = MESSAGE_TEXT;
+      error := (message || stack);
+      return;
+  END;
+
+  amount_purchased := catalog_purchase.quantity;
+
+  insert into potion_ledger_items (shop_id, tick_id, quantity_changed, potion_type)
+    values (shop_id, tick_id, -1 * amount_purchased, catalog_purchase.potion_type);
+  gold := amount_purchased * catalog_purchase.price;
+
+  insert into gold_ledger_items (shop_id, tick_id, gold_changed)
+    values (shop_id, tick_id, gold);
+END;
+$fn$;
+
 CREATE OR REPLACE PROCEDURE catalog_for_shop(api_url text, shop_id bigint, tick_id bigint, out error text)
 LANGUAGE plpgsql
 AS $$
@@ -47,8 +129,8 @@ BEGIN
   COMMIT;
 
   BEGIN
-  insert into catalog_items (sku, name, quantity, description, tick_id, shop_id)
-    select sku, name, quantity, description, tick_id, shop_id
+  insert into catalog_items (sku, name, quantity, potion_type, tick_id, shop_id, price)
+    select sku, name, quantity, potion_type, tick_id, shop_id, price
     from
     json_populate_recordset(null::record, result.content::json)
     AS
@@ -56,24 +138,26 @@ BEGIN
         sku text,
         name text,
         quantity smallint,
-        description text
+        price smallint,
+        potion_type vector(3)
     );
     EXCEPTION WHEN OTHERS THEN
       GET STACKED DIAGNOSTICS stack = PG_EXCEPTION_CONTEXT, message = MESSAGE_TEXT;
       error := (message || stack);
       return;
   END;
+
+  INSERT INTO public.jobs(shop_id, job_type, tick_id)
+  VALUES(shop_id, 'PURCHASE_POTIONS', tick_id);
 END;
 $$;
-
-
 
 CREATE OR REPLACE PROCEDURE run_jobs()
 LANGUAGE plpgsql
 AS $$
 DECLARE
   job jobs%ROWTYPE;
-  api_url text;
+  shop shops%ROWTYPE;
   error_message text;
   counter integer := 0;
 BEGIN
@@ -83,14 +167,23 @@ BEGIN
     SELECT * INTO job FROM jobs WHERE success IS NULL ORDER BY created_at LIMIT 1;
 
     if not found then
-      raise log 'No jobs to execute.';
+      raise LOG 'No jobs to execute.';
       return;
     end if;
 
-    api_url := (SELECT shops.api_url FROM shops WHERE id = job.shop_id);
-    
-    CALL catalog_for_shop(api_url, job.shop_id, job.tick_id, error_message);
+    SELECT * INTO shop FROM shops WHERE id = job.shop_id;
 
+    error_message := null;
+
+	  case 
+		  when job.job_type = 'REFRESH_CATALOG' then
+        CALL catalog_for_shop(shop.api_url, job.shop_id, job.tick_id, error_message);
+      when job.job_type = 'PURCHASE_POTIONS' then
+        CALL purchase_from_shop(shop.api_url, shop.api_key, job.shop_id, job.tick_id, error_message);
+      else
+        error_message := ('Unknown job type: ' || job.job_type);
+    end case;  
+    
     if error_message is null then
       UPDATE jobs SET success = TRUE WHERE id = job.id;
     else
@@ -143,6 +236,8 @@ create table
     constraint potion_ledger_items_tick_id_fkey foreign key (tick_id) references ticks (id)
   ) tablespace pg_default;
 
+DROP TABLE jobs;
+
   create table
   public.jobs (
     id bigint generated by default as identity not null,
@@ -156,7 +251,8 @@ create table
     constraint jobs_shop_id_fkey foreign key (shop_id) references shops (id)
   ) tablespace pg_default;
 
-
+  DROP TABLE catalog_items;
+  
   create table
   public.catalog_items (
     id bigint generated by default as identity not null,
@@ -164,7 +260,8 @@ create table
     sku text null,
     name text null,
     quantity smallint not null,
-    description text null,
+    potion_type vector(3) not null,
+    price smallint not null,
     tick_id bigint not null,
     shop_id bigint not null default '1'::bigint,
     constraint catalog_shop_id_fkey foreign key (shop_id) references shops (id),
