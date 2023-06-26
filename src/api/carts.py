b5c510a82fd55b4292f3e83335f2cb9e49329224
jackalnom <jackalnom@gmail.com>
2023-06-26 14:26:09 -0700

    Added additional cart endpoints

--- src/api/carts.py
+++ src/api/carts.py
@@ -8,9 +8,11 @@ router = APIRouter(
     dependencies=[Depends(auth.get_api_key)],
 )
 
+class NewCart(BaseModel):
+    customer: str
 
 @router.post("/")
-def create_cart(request: Request):
+def create_cart(request: Request, new_cart: NewCart):
     """ """
     if request.state.is_demo:
         return {"cart_id": 1, "is_demo": True}
@@ -39,9 +41,35 @@ def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
 
     return {"cart_id": 1}
 
+class CartCheckout(BaseModel):
+    payment: str
+    gold_paid: int
 
 @router.post("/{cart_id}/checkout")
-def checkout(cart_id: int):
+def checkout(cart_id: int, cart_checkout: CartCheckout):
+    """ """
+
+    return {"order_id": 1}
+
+class CartPickupItem(BaseModel):
+    potion_type: str
+    quantity: int
+
+class CartPickup(BaseModel):
+    # Description of items
+    items: list[CartPickupItem]
+
+@router.post("/{cart_id}/pickup")
+def pickup_order(cart_id: int, cart_pickup: CartPickup):
+    """ """
+
+    return {"order_id": 1}
+
+class CartRefund(BaseModel):
+    gold_refunded: int
+
+@router.post("/{cart_id}/refund")
+def refund_order(cart_id: int, cart_refund: CartRefund):
     """ """
 
     return {"order_id": 1}
