b58e74c61df4c5559de07c32e4658d6aa1603f48
jackalnom <jackalnom@gmail.com>
2023-07-04 15:26:42 -0700

    removed extra code

--- src/database.py
+++ src/database.py
@@ -2,15 +2,15 @@ import os
 import dotenv
 
 
-def database_connection_url():
-    dotenv.load_dotenv()
-    DB_USER: str = os.environ.get("POSTGRES_USER")
-    DB_PASSWD = os.environ.get("POSTGRES_PASSWORD")
-    DB_SERVER: str = os.environ.get("POSTGRES_SERVER")
-    DB_PORT: str = os.environ.get("POSTGRES_PORT")
-    DB_NAME: str = os.environ.get("POSTGRES_DB")
-    return f"postgresql://{DB_USER}:{DB_PASSWD}@{DB_SERVER}:{DB_PORT}/{DB_NAME}"
+# def database_connection_url():
+#     dotenv.load_dotenv()
+#     DB_USER: str = os.environ.get("POSTGRES_USER")
+#     DB_PASSWD = os.environ.get("POSTGRES_PASSWORD")
+#     DB_SERVER: str = os.environ.get("POSTGRES_SERVER")
+#     DB_PORT: str = os.environ.get("POSTGRES_PORT")
+#     DB_NAME: str = os.environ.get("POSTGRES_DB")
+#     return f"postgresql://{DB_USER}:{DB_PASSWD}@{DB_SERVER}:{DB_PORT}/{DB_NAME}"
 
 
-print(f"working directory: {os.getcwd()}")
-print(f"database_connection_url: {database_connection_url()}")
+# print(f"working directory: {os.getcwd()}")
+# print(f"database_connection_url: {database_connection_url()}")
