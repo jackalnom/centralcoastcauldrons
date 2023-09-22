fcc24effc734de5cbec14178f60efe3e7438bfea
Liam Kbidy <lkbidy@amazon.com>
2023-09-22 11:23:58 -0700

    Refactor database.py to src/

--- src/database.py
+++ src/database.py
@@ -2,9 +2,11 @@ import os
 import dotenv
 from sqlalchemy import create_engine
 
+
 def database_connection_url():
     dotenv.load_dotenv()
 
     return os.environ.get("POSTGRES_URI")
+
 
 engine = create_engine(database_connection_url(), pool_pre_ping=True)
