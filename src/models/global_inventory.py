381a55dcc83f4efc88449fe7ce3087f583f4f4fc
Alfred Madere <45337924+AlfredMadere@users.noreply.github.com>
2023-09-29 13:38:58 -0700

    fixed inventoary singleton to update itself every time its called

--- src/models/global_inventory.py
+++ src/models/global_inventory.py
@@ -34,40 +34,39 @@ class GlobalInventory:
         self.num_red_ml = num_red_ml
         self.gold = gold
 
+        print("GlobalInventory created", self)
+
     
     @staticmethod
     def get_singleton():
-        if GlobalInventory.singleton is None:
 
             #check to see if there is a row in the table
 
-            sql_to_execute = text(f"SELECT * FROM {GlobalInventory.table_name} LIMIT 1")
+        sql_to_execute = text(f"SELECT * FROM {GlobalInventory.table_name} LIMIT 1")
 
-            with db.engine.begin() as connection:
-                result = connection.execute(sql_to_execute)
-                row = result.fetchone()
+        with db.engine.begin() as connection:
+            result = connection.execute(sql_to_execute)
+            row = result.fetchone()
 
-            if row is not None:
-                # If a row exists, set the singleton to that row
-                GlobalInventory.singleton = GlobalInventory(*row)
-            else:
-                # If no row exists, create a row with default values
-                raise Exception("No global inventory row exists")
-                # with db.engine.begin() as connection:
-                #     sql_to_execute = (
-                #         f"INSERT INTO {GlobalInventory.table_name} (created_at, num_red_potions, num_red_ml, gold) "
-                #         f"VALUES (0, 0, 0, 0)"
-                #     )
-                #     connection.execute(sql_to_execute)
-                #     GlobalInventory.singleton = GlobalInventory(0, 0, 0, 0)
+        if row is not None:
+            # If a row exists, set the singleton to that row
+            GlobalInventory.singleton = GlobalInventory(*row)
+        else:
+            # If no row exists, create a row with default values
+            raise Exception("No global inventory row exists")
+            # with db.engine.begin() as connection:
+            #     sql_to_execute = (
+            #         f"INSERT INTO {GlobalInventory.table_name} (created_at, num_red_potions, num_red_ml, gold) "
+            #         f"VALUES (0, 0, 0, 0)"
+            #     )
+            #     connection.execute(sql_to_execute)
+            #     GlobalInventory.singleton = GlobalInventory(0, 0, 0, 0)
 
-        return GlobalInventory.singleton
             #if there is, then set the singleton to that row
             #if there isn't, then create a row with the default values
-
+        return GlobalInventory.singleton
+    
     def get_catalog(self):
-        if(self.num_red_potions == 0):
-            return []
         
         return [
             {
