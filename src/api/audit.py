9c3dbff29c0b3d1f487cafd39619157b8ca78e43
Matthew Wong <61444031+cup0noodles@users.noreply.github.com>
2023-09-29 20:01:34 -0700

    implimented audit system

--- src/api/audit.py
+++ src/api/audit.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from src.api import auth
 import math
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/audit",
@@ -12,8 +14,15 @@ router = APIRouter(
 @router.get("/inventory")
 def get_inventory():
     """ """
-    
-    return {"number_of_potions": 0, "ml_in_barrels": 0, "gold": 0}
+    print("Audit in Progress...")
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
+        for row in result:
+            current_red = row[1]
+            current_ml = row[2]
+            current_gold = row[3]
+    print(f"Current Stats: Potions-{current_red}, Stock in mL-{current_ml}, Gold-{current_gold}")
+    return {"number_of_potions": current_red, "ml_in_barrels": current_ml, "gold": current_gold}
 
 class Result(BaseModel):
     gold_match: bool
