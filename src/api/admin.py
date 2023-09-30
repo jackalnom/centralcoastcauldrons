7e70506837f3eff9401b4fc5695c6de3e2f0b86a
Matthew Wong <61444031+cup0noodles@users.noreply.github.com>
2023-09-29 18:55:02 -0700

    updated reset

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 from src.api import auth
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/admin",
@@ -14,6 +16,10 @@ def reset():
     Reset the game state. Gold goes to 100, all potions are removed from
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_red_ml = 0"))
+        result = connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_red_potions = 0"))
+        result = connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold = 100"))
     return "OK"
 
 
