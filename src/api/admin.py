c262743531d6d200d7dda5b9e2e8a66f60b9e683
dphun123 <dennisphun321@gmail.com>
2023-09-30 18:20:46 -0700

    set up resetting and shop names

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,29 +1,33 @@
 from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 from src.api import auth
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
-    prefix="/admin",
-    tags=["admin"],
-    dependencies=[Depends(auth.get_api_key)],
+  prefix="/admin",
+  tags=["admin"],
+  dependencies=[Depends(auth.get_api_key)],
 )
 
 @router.post("/reset")
 def reset():
-    """
-    Reset the game state. Gold goes to 100, all potions are removed from
-    inventory, and all barrels are removed from inventory. Carts are all reset.
-    """
-    return "OK"
+  """
+  Reset the game state. Gold goes to 100, all potions are removed from
+  inventory, and all barrels are removed from inventory. Carts are all reset.
+  """
+  with db.engine.begin() as connection:
+    connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold=100, num_red_ml=0, num_red_potions=0"))
+  return "OK"
 
 
 @router.get("/shop_info/")
 def get_shop_info():
-    """ """
+  """ """
 
-    # TODO: Change me!
-    return {
-        "shop_name": "Potion Shop",
-        "shop_owner": "Potion Seller",
-    }
+  # TODO: Change me!
+  return {
+      "shop_name": "phun potions",
+      "shop_owner": "Dennis Phun",
+  }
 
