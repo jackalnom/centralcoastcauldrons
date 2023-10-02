970a93480a6d1c8cb2abf36411a96ac72fbaf584
Matthew Wong <61444031+cup0noodles@users.noreply.github.com>
2023-10-01 23:33:43 -0700

    undid catalog changes

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -20,39 +20,15 @@ def get_catalog():
         for row in result:
             quantity_red = row[1]
     return_list = []
-    CATALOG_OFFER_SIZE = 3
-    sku_count = 0
-    while quantity_red > 0:
-        if quantity_red >= CATALOG_OFFER_SIZE:
-            # more than desired split size
-            return_list += [{
-                "sku": f"RED_POTION_{sku_count}",
-                "name": "red potion",
-                "quantity": CATALOG_OFFER_SIZE,
-                "price": 50,
-                "potion_type": [100, 0, 0, 0],
-            }]
-            quantity_red -= CATALOG_OFFER_SIZE
-            sku_count += 1
-        else:
-            # less than desired split size
-            return_list += [{
-                "sku": f"RED_POTION_{sku_count}",
+    if quantity_red > 0:
+        print(f"Catalog contains {quantity_red} Red Potions...")
+        return_list += [
+            {
+                "sku": "RED_POTION_0",
                 "name": "red potion",
                 "quantity": quantity_red,
                 "price": 50,
                 "potion_type": [100, 0, 0, 0],
-            }]
-            quantity_red = 0
-    # if quantity_red > 0:
-    #     print(f"Catalog contains {quantity_red} Red Potions...")
-    #     return [
-    #         {
-    #             "sku": "RED_POTION_0",
-    #             "name": "red potion",
-    #             "quantity": quantity_red,
-    #             "price": 50,
-    #             "potion_type": [100, 0, 0, 0],
-    #         }
-    #     ]
+            }
+        ]
     return return_list
