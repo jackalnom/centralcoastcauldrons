4cbd019170f539eccb034eb4e70b1e3a4f2f33a5
dphun123 <dennisphun321@gmail.com>
2023-10-01 18:43:44 -0700

    set up checkout for only red potions

--- src/api/carts.py
+++ src/api/carts.py
@@ -1,47 +1,53 @@
 from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 from src.api import auth
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
-    prefix="/carts",
-    tags=["cart"],
-    dependencies=[Depends(auth.get_api_key)],
+  prefix="/carts",
+  tags=["cart"],
+  dependencies=[Depends(auth.get_api_key)],
 )
 
-
 class NewCart(BaseModel):
-    customer: str
+  customer: str
 
 
 @router.post("/")
 def create_cart(new_cart: NewCart):
-    """ """
-    return {"cart_id": 1}
+  """ """
+  return {"cart_id": 1}
 
 
 @router.get("/{cart_id}")
 def get_cart(cart_id: int):
-    """ """
+  """ """
 
-    return {}
+  return {}
 
 
 class CartItem(BaseModel):
-    quantity: int
+  quantity: int
 
 
 @router.post("/{cart_id}/items/{item_sku}")
 def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
-    """ """
+  """ """
 
-    return "OK"
+  return "OK"
 
 
 class CartCheckout(BaseModel):
-    payment: str
+  payment: str
 
 @router.post("/{cart_id}/checkout")
 def checkout(cart_id: int, cart_checkout: CartCheckout):
-    """ """
-
-    return {"total_potions_bought": 1, "total_gold_paid": 50}
+  """ """
+  with db.engine.begin() as connection:
+    result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
+    first_row = result.first()
+    if first_row.num_red_potions != 0:
+      num_bought = first_row.num_red_potions if first_row.num_red_potions <= 20 else 20
+      return {"total_potions_bought": num_bought, "total_gold_paid": num_bought * 50}
+  return {}
