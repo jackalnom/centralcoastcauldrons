18e3dcad2fb966bef84150b3a871231ec41f6c24
dphun123 <dennisphun321@gmail.com>
2023-10-02 18:31:51 -0700

    updated reset

--- src/api/admin.py
+++ src/api/admin.py
@@ -17,7 +17,8 @@ def reset():
   inventory, and all barrels are removed from inventory. Carts are all reset.
   """
   with db.engine.begin() as connection:
-    connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold=100, num_red_ml=0, num_red_potions=0"))
+    connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold=100, num_red_potions=0, num_red_ml=0, \
+                                       num_green_potions=0, num_green_ml=0, num_blue_potions=0, num_blue_ml=0"))
   return "OK"
 
 
