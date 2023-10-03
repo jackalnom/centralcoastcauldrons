13e70e9b7d31ff8aca3c592fbd54bb1aa4ae4f45
dphun123 <dennisphun321@gmail.com>
2023-10-02 18:06:21 -0700

    updated barrel plan to take green and blue

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -40,15 +40,24 @@ def post_deliver_barrels(barrels_delivered: list[Barrel]):
 def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
   """ """
   print(wholesale_catalog)
+  buying_barrels = []
+  color_to_potion = {
+    "red": [1, 0, 0, 0],
+    "green": [0, 1, 0, 0],
+    "blue": [0, 0, 1, 0],
+  }
   with db.engine.begin() as connection:
     result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
     first_row = result.first()
-    for barrel in wholesale_catalog:
-      if barrel.potion_type == [1, 0, 0, 0] and first_row.num_red_potions < 10 and first_row.gold >= barrel.price:
-        return [
-          {
-            "sku": barrel.sku,
-            "quantity": 1,
-          }
-        ]
-  return []
+    current_gold = first_row.gold
+    for color in ["red", "green", "blue"]:
+      if getattr(first_row, f"num_{color}_potions") < 10:
+        for barrel in wholesale_catalog:
+          if barrel.potion_type == color_to_potion[color] and current_gold >= barrel.price:
+            buying_barrels.append({
+              "sku": barrel.sku,
+              "quantity": 1,
+            })
+            current_gold -= barrel.price
+            break
+  return buying_barrels
