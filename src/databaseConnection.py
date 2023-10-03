6ddc2b17176e2c793b62da2662f5a0926ece6a2c
Luke Fanguna <47843203+Luke-Fanguna@users.noreply.github.com>
2023-10-02 18:40:39 -0700

    Create databaseConnection.py

---
+++ src/databaseConnection.py
@@ -0,0 +1,10 @@
+import os
+import dotenv
+from sqlalchemy import create_engine
+
+def database_connection_url():
+    dotenv.load_dotenv()
+
+    return os.environ.get("POSTGRES_URI")
+
+engine = create_engine(database_connection_url(), pool_pre_ping=True)
