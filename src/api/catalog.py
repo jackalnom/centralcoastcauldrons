33ee162f8840d47c38cb80aff37e11808e74be0e
dphun123 <dennisphun321@gmail.com>
2023-10-03 10:38:06 -0700

    updated catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -4,6 +4,12 @@ from src import database as db
 
 router = APIRouter()
 
+colors = ["red", "green", "blue"]
+color_to_potion = {
+    "red": [100, 0, 0, 0],
+    "green": [0, 100, 0, 0],
+    "blue": [0, 0, 100, 0],
+}
 
 @router.get("/catalog/", tags=["catalog"])
 def get_catalog():
@@ -15,14 +21,15 @@ def get_catalog():
   with db.engine.begin() as connection:
     result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
     first_row = result.first()
-    if first_row.num_red_potions != 0:
-      return [
-        {
-            "sku": "RED_POTION_0",
-            "name": "red potion",
-            "quantity": first_row.num_red_potions if first_row.num_red_potions <= 10000 else 10000,
-            "price": 50,
-            "potion_type": [100, 0, 0, 0],
-        }
-      ]
-  return []
+    catalog = []
+    for color in colors:
+      current_potions = getattr(first_row, f"num_{color}_potions")
+      if current_potions != 0:
+        catalog.append({
+          "sku": f"{color.upper()}_POTION_0",
+          "name": f"{color} potion",
+          "quantity": current_potions if current_potions <= 10000 else 10000,
+          "price": 50,
+          "potion_type": color_to_potion[color],
+        })
+  return catalog
