5c0d51a96ac6ca13904ff2387606d4ca30eb1c0b
dphun123 <dennisphun321@gmail.com>
2023-10-08 19:55:52 -0700

    fixed reset

--- src/api/admin.py
+++ src/api/admin.py
@@ -22,7 +22,7 @@ def reset():
   with db.engine.begin() as connection:
     connection.execute(sqlalchemy.text("""
         UPDATE global_inventory
-        SET gold=100, num_red_potions=0, num_red_ml=0, num_green_potions=0, num_green_ml=0, num_blue_potions=0, num_blue_ml=0
+        SET gold=100, num_red_ml=0, num_green_ml=0, num_blue_ml=0, num_dark_ml=0
         """))
   delete_table("potion_inventory")
   delete_table("carts")
