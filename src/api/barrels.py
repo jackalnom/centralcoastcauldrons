55444894c15104d22d2921201bf4163b3221ecef
mkw_pop <mattkadenwong@gmail.com>
2023-10-11 14:16:19 -0700

    barrel optimization

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -29,29 +29,31 @@ def post_deliver_barrels(barrels_delivered: list[Barrel]):
         2: "blue",
         3: "dark"
     }
-    SKIP_COLOR_KEY = "SKIP"
     for indiv_barrel in barrels_delivered:
         ml_total_delivered=0
         cost_total=0
         current_gold=0
         current_ml=0
-        color = delivery_dict.get(indiv_barrel.potion_type.index(1), SKIP_COLOR_KEY)
-        if(color != "SKIP"):
-            ml_total_delivered = indiv_barrel.quantity*indiv_barrel.ml_per_barrel
-            cost_total = indiv_barrel.quantity*indiv_barrel.price
-            with db.engine.begin() as connection:
-                result_gold = connection.execute(sqlalchemy.text("SELECT gold FROM global_inventory"))
-                result_ml = connection.execute(sqlalchemy.text(f"SELECT num_{color}_ml FROM global_inventory"))
-            for row in result_gold:
-                current_gold = row[0] - cost_total
-            for row in result_ml:
-                current_ml = row[0] + ml_total_delivered
-            
-            print(f"Delivery taken of {ml_total_delivered}mL of {color} potion, at cost of {cost_total}.")
-            print(f"Current {color} potion stock is {current_ml}mL, current gold is {current_gold}")
-            with db.engine.begin() as connection:
-                result = connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_{color}_ml = {current_ml}"))
-                result = connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET gold = {current_gold}"))
+
+        color = delivery_dict.get(indiv_barrel.potion_type.index(1))
+        
+        ml_total_delivered = indiv_barrel.quantity*indiv_barrel.ml_per_barrel
+        cost_total = indiv_barrel.quantity*indiv_barrel.price
+
+        with db.engine.begin() as connection:
+            result = connection.execute(sqlalchemy.text(f"SELECT gold, num_{color}_ml \
+                                                          FROM global_inventory"))
+        row = result.first()
+        current_gold = row[0] - cost_total
+        current_ml = row[0] + ml_total_delivered
+        
+        print(f"Delivery taken of {ml_total_delivered}mL of {color} potion, at cost of {cost_total}.")
+        print(f"Current {color} potion stock is {current_ml}mL, current gold is {current_gold}")
+        
+        with db.engine.begin() as connection:
+            result = connection.execute(sqlalchemy.text(f"UPDATE global_inventory \
+                                                          SET num_{color}_ml = {current_ml}, gold = {current_gold}"))
+
 
     ## end new implimentation 
     print(barrels_delivered)
@@ -68,14 +70,14 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     purchase_plan = []
     # add entries as more barrels are desired
     purchasing_dict = {
-         "MINI_RED_BARREL": "red",
+        "SMALL_RED_BARREL": "red",
         # "SMALL_GREEN_BARREL": "green",
         # "MINI_BLUE_BARREL": "blue",
         # "SMALL_DARK_BARREL": "dark"
     }
     SKIP_COLOR_KEY = "SKIP"
     for for_sale in wholesale_catalog:  # go through catalog
-        print("Going through catalog...")
+        #print("Going through catalog...")
         color = purchasing_dict.get(for_sale.sku, SKIP_COLOR_KEY)
         if color == SKIP_COLOR_KEY:
             # skip if not small barrel
