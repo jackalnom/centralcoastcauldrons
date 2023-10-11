6b0bcacd8e5f3f768e7aa90f8288ffed0f41e433
Mihir <mihir1003@gmail.com>
2023-10-11 07:29:16 -0700

    cheap

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -21,7 +21,7 @@ def get_catalog():
                 "sku": "RED_POTION_0",
                 "name": "red potion",
                 "quantity": num_red_potions,
-                "price": 50,
+                "price": 10,
                 "potion_type": [100, 0, 0, 0],
             })
         if num_green_potions > 0:
@@ -29,7 +29,7 @@ def get_catalog():
                 "sku": "GREEN_POTION_0",
                 "name": "green potion",
                 "quantity": num_green_potions,
-                "price": 50,
+                "price": 1,
                 "potion_type": [0, 100, 0, 0],
             })
         if num_blue_potions > 0:
@@ -37,7 +37,7 @@ def get_catalog():
                 "sku": "BLUE_POTION_0",
                 "name": "blue potion",
                 "quantity": num_blue_potions,
-                "price": 50,
+                "price": 1,
                 "potion_type": [0, 0, 100, 0],
             })
         return catalog
