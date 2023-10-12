0e3c34575ee082f1764a988b05037771b725f2f0
Matthew Wong <61444031+cup0noodles@users.noreply.github.com>
2023-10-11 23:58:55 -0700

    fixed bug in barrel deliver

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -45,7 +45,7 @@ def post_deliver_barrels(barrels_delivered: list[Barrel]):
                                                           FROM global_inventory"))
         row = result.first()
         current_gold = row[0] - cost_total
-        current_ml = row[0] + ml_total_delivered
+        current_ml = row[1] + ml_total_delivered
         
         print(f"Delivery taken of {ml_total_delivered}mL of {color} potion, at cost of {cost_total}.")
         print(f"Current {color} potion stock is {current_ml}mL, current gold is {current_gold}")
@@ -65,8 +65,59 @@ def post_deliver_barrels(barrels_delivered: list[Barrel]):
 def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     """ """
     print(wholesale_catalog)
-   
-    # Start new implimentation
+    
+    #----------------------------------
+    # -- Log Barrels -- 
+    #----------------------------------
+    
+    # Create new session ID
+    with db.engine.begin() as connection:
+        catalog_id_result = connection.execute(sqlalchemy.text(f"INSERT INTO barrels_catalog \
+                                                                DEFAULT VALUES  \
+                                                                RETURNS catalog_id"))
+        catalog_id =catalog_id_result.first()[0]
+
+    # Go through and log all barrels for sale
+    for barrel in wholesale_catalog:
+        with db.engine.begin() as connection:
+            # check if sku exists in table already
+            sku_exist_result = connection.execute(sqlalchemy.text(f"SELECT COUNT(sku) \
+                                                                    FROM barrels_catalog \
+                                                                    WHERE sku = '{barrel.sku}'"))
+        if not sku_exist_result.first()[0]:
+            # if not create entry
+            sql_getid = f"INSERT INTO barrels_catalog \
+                          (sku, quantity) \
+                          VALUES ('{barrel.sku}, {barrel.quantity}') \
+                          RETURNS id"
+        else:
+            sql_getid = f"SELECT id \
+                          FROM barrels_catalog \
+                          WHERE sku = '{barrel.sku}'"
+
+        with db.engine.begin() as connection:
+            result = connection.execute(sqlalchemy.text(sql_getid))
+        barrel_id = result.first()[0]
+
+        # Now log actual price for tracking
+        with db.engine.begin() as connection:
+            # check if sku exists in table already
+            sku_exist_result = connection.execute(sqlalchemy.text(f"INSERT INTO barrels_history \
+                                                                    (catalog_id, barrel_id, cost) \
+                                                                    VALUES ({catalog_id}, {barrel_id}, {barrel.price})"))
+    #----------------------------------
+    # -- Build Purchase Plan -- 
+    #----------------------------------
+
+    # get current potion stock levels
+    with db.engine.begin() as connection:
+        check_stock_result = connection.execute(sqlalchemy.text(f"SELECT FROM potion_inventory \
+                                                                  type_red, type_green, type_blue, type_dark, quantity \
+                                                                  WHERE quantity != 0"))
+    total_stock = [0,0,0,0]
+    for potion in check_stock_result:
+        quantity = potion.pop()
+        
     purchase_plan = []
     # add entries as more barrels are desired
     purchasing_dict = {
@@ -76,6 +127,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
         # "SMALL_DARK_BARREL": "dark"
     }
     SKIP_COLOR_KEY = "SKIP"
+    
     for for_sale in wholesale_catalog:  # go through catalog
         #print("Going through catalog...")
         color = purchasing_dict.get(for_sale.sku, SKIP_COLOR_KEY)
