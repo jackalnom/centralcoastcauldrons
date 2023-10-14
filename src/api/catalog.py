66090c100ebdb84aed178e14826cb105943ea210
Mihir <mihir1003@gmail.com>
2023-10-14 14:16:35 -0700

    remove logging

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -24,7 +24,7 @@ def get_catalog():
             "sku": "RED_POTION_0",
             "name": "red potion",
             "quantity": inventory.num_red_potions,
-            "price": 20,
+            "price": 30,
             "potion_type": [100, 0, 0, 0],
         })
     if inventory.num_green_potions > 0:
