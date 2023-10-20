bfb51de00f0d3fd210775415742eea1778c49e8e
dphun123 <dennisphun321@gmail.com>
2023-10-20 16:39:51 -0700

    updated audit to use ledgers

--- src/api/audit.py
+++ src/api/audit.py
@@ -18,17 +18,16 @@ def get_inventory():
     """ """
     with db.engine.begin() as connection:
       global_inventory = connection.execute(sqlalchemy.text("""
-          SELECT
-            gold AS gold,
-            num_red_ml + num_green_ml + num_blue_ml + num_dark_ml AS ml_in_barrels
-          FROM global_inventory
-          """)).first()
-      potion_inventory = connection.execute(sqlalchemy.text("""
-          SELECT
-            SUM(num_potion) AS number_of_potions
-          FROM potion_inventory
-          """)).first()
-    return {"number_of_potions": potion_inventory.number_of_potions, "ml_in_barrels": global_inventory.ml_in_barrels, "gold": global_inventory.gold}
+        SELECT
+          SUM(change_gold) as gold, 
+          SUM(change_red_ml + change_green_ml + change_blue_ml + change_dark_ml) as ml_in_barrels
+        FROM global_inventory_entries
+        """)).first()
+      number_of_potions = connection.execute(sqlalchemy.text("""
+        SELECT COALESCE(SUM(change), 0) as number_of_potions
+        FROM potion_entries
+        """)).first().number_of_potions
+    return {"number_of_potions": number_of_potions, "ml_in_barrels": global_inventory.ml_in_barrels, "gold": global_inventory.gold}
 
 
 class Result(BaseModel):
