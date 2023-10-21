f5c99fc0ffd613c78b900b781c278c87ca776196
Lucas Pierce <jackalnom@gmail.com>
2023-10-20 20:29:39 -0700

    fix allow headers

--- src/api/server.py
+++ src/api/server.py
@@ -29,7 +29,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["GET", "OPTIONS"],
-    allow_headers=[""],
+    allow_headers=["*"],
 )
 
 app.include_router(audit.router)
