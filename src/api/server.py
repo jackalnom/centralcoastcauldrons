ddbaf1394617acd9b48cc91a8f9f8c20b26e91df
Lucas Pierce <jackalnom@gmail.com>
2023-10-20 20:06:10 -0700

    adding options to CORS

--- src/api/server.py
+++ src/api/server.py
@@ -28,7 +28,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=False,
-    allow_methods=["GET"],
+    allow_methods=["GET", "OPTIONS"],
     allow_headers=[""],
 )
 
