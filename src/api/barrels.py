a473a9fba5e8f38ea8f30a0271b8023555d7fef6
liamhyde02 <73969644+liamhyde02@users.noreply.github.com>
2024-04-10 12:45:48 -0700

    surely this will fix my purchasing plan

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -48,22 +48,46 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text(sql_to_execute))
         row = result.fetchone()._asdict()
-        quantity = 0
-        sku = ""
-        current_ml = 0
+        barrels_to_purchase = []
         for barrel in wholesale_catalog:
             if barrel.potion_type == [0, 1, 0, 0]:
                 if row["num_green_potions"] < 10:
-                    if barrel.price <= row["gold"]:
-                        if barrel.ml_per_barrel > current_ml:
-                            current_ml = barrel.ml_per_barrel
-                            quantity = 1
-                            sku = barrel.sku
-        if quantity == 0:
-            return []
-        return [
-            {
-                "sku": sku,
-                "quantity": quantity,
-            }
-        ]
+                    barrels_to_purchase.append(barrel)
+            # elif barrel.potion_type == [1, 0, 0, 0]:
+            #     if row["num_red_potions"] < 10:
+            #         barrels_to_purchase.append(barrel)
+            # elif barrel.potion_type == [0, 0, 1, 0]:
+            #     if row["num_blue_potions"] < 10:
+            #         barrels_to_purchase.append(barrel)
+        current_green_barrel = None
+        # current_red_barrel = None
+        # current_blue_barrel = None
+        green_running_total = 0
+        red_running_total = 0
+        blue_running_total = 0
+        print(barrels_to_purchase)
+        print(row["gold"])
+        for barrel in barrels_to_purchase:
+            if red_running_total + blue_running_total + barrel.price <= row["gold"]:
+                if barrel.potion_type == [0, 1, 0, 0]:
+                    if current_green_barrel:
+                        print("reached green barrel comparison")
+                        if barrel.ml_per_barrel > current_green_barrel.ml_per_barrel:
+                            green_running_total += barrel.price
+                            green_running_total -= current_green_barrel.price
+                            current_green_barrel = barrel
+                    else:
+                        current_green_barrel = barrel
+                # elif barrel.potion_type == [1, 0, 0, 0]:
+                #     current_red_barrel = barrel
+                # elif barrel.potion_type == [0, 0, 1, 0]:
+                #     current_blue_barrel = barrel
+        purchase_plan = []
+        if current_green_barrel:
+            purchase_plan.append(
+                {
+                    "sku": current_green_barrel.sku,
+                    "quantity": 1,
+                }
+            )
+        return purchase_plan
