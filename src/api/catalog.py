d2ba6d946e1617bcc3cec3b9145162768c76b43e
liamhyde02 <73969644+liamhyde02@users.noreply.github.com>
2024-04-10 13:20:58 -0700

    patched catalog again

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -13,7 +13,7 @@ def get_catalog():
     sql_to_execute = "SELECT * FROM global_inventory"
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text(sql_to_execute))
-        row = result.fetchall().asdict()
+        row = result.fetchone()._asdict()
         results = []
         if (row["num_green_potions"]) > 0:
             results.append(
@@ -21,8 +21,28 @@ def get_catalog():
                     "sku": "GREEN_POTION",
                     "name": "Small Green Potion",
                     "quantity": row["num_green_potions"],
-                    "price": 50,
+                    "price": 45,
                     "potion_type": [0, 100, 0, 0],
+                }
+            )
+        elif (row["num_red_potions"]) > 0:
+            results.append(
+                {
+                    "sku": "RED_POTION",
+                    "name": "Small Red Potion",
+                    "quantity": row["num_red_potions"],
+                    "price": 45,
+                    "potion_type": [100, 0, 0, 0],
+                }
+            )
+        elif (row["num_blue_potions"]) > 0:
+            results.append(
+                {
+                    "sku": "BLUE_POTION",
+                    "name": "Small Blue Potion",
+                    "quantity": row["num_blue_potions"],
+                    "price": 45,
+                    "potion_type": [0, 0, 100, 0],
                 }
             )
         return results
