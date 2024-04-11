77b811655a565baaddcd8a0070380f4a7ad67903
liamhyde02 <73969644+liamhyde02@users.noreply.github.com>
2024-04-10 23:41:03 -0700

    updated reset oops

--- src/api/admin.py
+++ src/api/admin.py
@@ -16,14 +16,16 @@ def reset():
     Reset the game state. Gold goes to 100, all potions are removed from
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
-    sql_to_execute = f"UPDATE global_inventory SET num_green_potions = 0, num_green_ml = 0, num_red_potions = 0, num_red_ml = 0, num_blue_potions = 0, num_blue_ml = 0, gold = 100"
+    potion_catalog_sql = f"UPDATE potion_catalog_items SET quantity = 0"
+    barrels_sql = f"UPDATE barrel_inventory SET potion_ml = 0"
+    carts_sql = f"DELETE FROM carts"
+    cart_items_sql = f"DELETE FROM cart_items"
+    global_inventory_sql = f"UPDATE global_inventory SET gold = 100, cart_id = 0"
     with db.engine.begin() as connection:
-        connection.execute(sqlalchemy.text(sql_to_execute))
-    sql_to_execute = f"DELETE FROM barrel_purchases"
-    with db.engine.begin() as connection:
-        connection.execute(sqlalchemy.text(sql_to_execute))
-    sql_to_execute = f"DELETE FROM carts"
-    with db.engine.begin() as connection:
-        connection.execute(sqlalchemy.text(sql_to_execute))
+        connection.execute(sqlalchemy.text(potion_catalog_sql))
+        connection.execute(sqlalchemy.text(barrels_sql))
+        connection.execute(sqlalchemy.text(carts_sql))
+        connection.execute(sqlalchemy.text(cart_items_sql))
+        connection.execute(sqlalchemy.text(global_inventory_sql))
     return "OK"
 
