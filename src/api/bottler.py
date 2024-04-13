6264e57258e72138fb3dd3670640813e60618765
liamhyde02 <73969644+liamhyde02@users.noreply.github.com>
2024-04-12 22:03:39 -0700

    I think implemented selling mixed potions?

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -5,6 +5,7 @@ from src.api import auth
 import sqlalchemy
 from src import database as db
 from src.api.helpers import potion_type_tostr
+from src.api.helpers import list_floor_division
 router = APIRouter(
     prefix="/bottler",
     tags=["bottler"],
@@ -21,10 +22,15 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int
     print(f"potions delivered: {potions_delivered} order_id: {order_id}")
     with db.engine.begin() as connection:
         for potion in potions_delivered:
-            sql_to_execute = f"UPDATE barrel_inventory SET potion_ml = potion_ml - {potion.quantity * 100} WHERE potion_type = '{potion_type_tostr(potion.potion_type)}'"
-            connection.execute(sqlalchemy.text(sql_to_execute))
-            sql_to_execute = f"UPDATE potion_catalog_items SET quantity = quantity + {potion.quantity} WHERE potion_type = '{potion_type_tostr(potion.potion_type)}'"
-            connection.execute(sqlalchemy.text(sql_to_execute))
+            for i in range(4):
+                barrel_type = [0 for _ in range(4)]
+                barrel_type[i] = 1
+                if potion.potion_type[i] == 0:
+                    continue
+                barrel_update_sql = f"UPDATE barrel_inventory SET potion_ml = potion_ml - {potion.quantity * potion.potion_type[i]} WHERE barrel_type = '{potion_type_tostr(barrel_type)}'"
+                connection.execute(sqlalchemy.text(barrel_update_sql))
+            gold_update_sql = f"UPDATE potion_catalog_items SET quantity = quantity + {potion.quantity} WHERE potion_type = '{potion_type_tostr(potion.potion_type)}'"
+            connection.execute(sqlalchemy.text(gold_update_sql))
     return "OK"
 
 @router.post("/plan")
@@ -36,19 +42,26 @@ def get_bottle_plan():
     # Each bottle has a quantity of what proportion of red, blue, and
     # green potion to add.
     # Expressed in integers from 1 to 100 that must sum up to 100.
-
-    sql_to_execute = "SELECT * FROM barrel_inventory WHERE potion_ml > 0"
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text(sql_to_execute))
+        barrel_inventory_sql = "SELECT * FROM barrel_inventory"
+        result = connection.execute(sqlalchemy.text(barrel_inventory_sql))
         rows = result.fetchall()
+        ml_inventory = [row._asdict()["potion_ml"] for row in rows]
+        print(f"ml_inventory: {ml_inventory}")
+        potion_catalog_sql = "SELECT * FROM potion_catalog_items"
+        result = connection.execute(sqlalchemy.text(potion_catalog_sql))
+        potions = result.fetchall()
+        potions.sort(key=lambda x: x.price, reverse=True)
         bottling_plan = []
-        for row in rows:
-            row = row._asdict()
-            bottling_plan.append(
-                {
-                    "potion_type": row["potion_type"],
-                    "quantity": row["potion_ml"] // 100,
-                }
-            )
+        for potion in potions:
+            potion = potion._asdict()
+            quantity = list_floor_division(ml_inventory, potion["potion_type"])
+            ml_inventory = [ml_inventory[i] - quantity * potion["potion_type"][i] for i in range(4)]
+            if quantity > 0:
+                bottling_plan.append(
+                                    {"potion_type": potion["potion_type"], 
+                                        "quantity": quantity
+                                    }
+                            )
         print(f"bottling_plan: {bottling_plan}")
         return bottling_plan
--- src/api/helpers.py
+++ src/api/helpers.py
@@ -1,2 +1,12 @@
 def potion_type_tostr(potion_type):
     return "{" + ",".join(map(str, potion_type)) + "}"
+
+def list_floor_division(list1, list2):
+    if len(list1) != len(list2):
+        raise ValueError("Lists must be of the same length")
+    available = []
+    for i in range(len(list1)):
+        if list2[i] == 0:
+            continue
+        available.append(list1[i] // list2[i])
+    return min(available)
