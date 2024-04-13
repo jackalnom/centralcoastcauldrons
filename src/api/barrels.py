be5c67fead0dce65ccf43f2df87f0d6d72a15441
liamhyde02 <73969644+liamhyde02@users.noreply.github.com>
2024-04-12 22:32:10 -0700

    updated barrel plan to prioritize most expensive potions!

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -39,33 +39,40 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     print(wholesale_catalog)
 
     with db.engine.begin() as connection:
-        barrels_to_purchase = []
-        for barrel in wholesale_catalog:
-            potion_type = [barrel.potion_type[0] * 100, barrel.potion_type[1] * 100, barrel.potion_type[2] * 100, barrel.potion_type[3] * 100]
-            sql_to_execute = f"SELECT * FROM potion_catalog_items WHERE potion_type = '{potion_type_tostr(potion_type)}'"
-            result = connection.execute(sqlalchemy.text(sql_to_execute))
-            potion = result.fetchone()
-            if potion is None:
-                continue
-            potion = potion._asdict()
-            if potion["quantity"] < 10:
-                barrels_to_purchase.append(barrel)
-
-        barrels = []
-        barrels_to_purchase.sort(key=lambda x: x.ml_per_barrel/barrel.price, reverse=True)
-        sql_to_execute = "SELECT * FROM global_inventory"
-        result = connection.execute(sqlalchemy.text(sql_to_execute))
+        gold_sql = "SELECT * FROM global_inventory"
+        result = connection.execute(sqlalchemy.text(gold_sql))
         global_inventory = result.fetchone()._asdict()
         running_total = global_inventory["gold"]
+        wholesale_catalog.sort(key=lambda x: x.ml_per_barrel/x.price, reverse=True)
+        barrel_plan = []
 
-        for barrel in barrels_to_purchase:
-            if barrel.price <= running_total:
-                running_total -= barrel.price
-                barrels.append(
-                    {
-                        "sku": barrel.sku,
-                        "quantity": 1,
-                    }
-                )
-        print(f"barrels: {barrels}")
-        return barrels
+        for barrel in wholesale_catalog:
+            barrel_bought = False
+            if barrel.price > running_total:
+                continue
+            print(f"barrel: {barrel} running_total: {running_total}")
+            potion_catalog_sql = f'SELECT * FROM potion_catalog_items'
+            result = connection.execute(sqlalchemy.text(potion_catalog_sql))
+            potions = result.fetchall()
+            potions.sort(key=lambda x: x.price, reverse=True)
+            for potion in potions:
+                potion = potion._asdict()
+                if potion["quantity"] < 10:
+                    for i in range(4):
+                        if potion["potion_type"][i] == 0:
+                            continue
+                        if barrel.potion_type[i] == 0:
+                            continue
+                        barrel_plan.append(
+                            {
+                                "sku": barrel.sku,
+                                "quantity": 1,
+                            }
+                        )
+                        running_total -= barrel.price
+                        barrel_bought = True
+                        break
+                if barrel_bought:
+                    break
+        print(f"barrel purchase plan: {barrel_plan}, running_total: {running_total}")
+        return barrel_plan
