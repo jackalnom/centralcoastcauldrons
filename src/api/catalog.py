14e0b5ef971ebba904c1fac51dfa3d357b0b9942
Xavier Garcia <xmangarcia17@gmail.com>
2024-04-13 11:27:41 -0700

    lower price to hopefully begin seeing purchases

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -34,7 +34,7 @@ def get_catalog():
                 "sku": "GREEN_POTION_0",
                 "name": "green potion",
                 "quantity": num,
-                "price": 50,
+                "price": 30,
                 "potion_type": [0, 100, 0, 0]
             }
         ]
