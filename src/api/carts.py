db7d1537d76dce7520e8f043281f4ce32b007ea9
Jesus Avalos <jesusavalosr2004@gmail.com>
2024-04-14 14:30:04 -0700

    carts: insert / create cart, insert potion quantity

--- src/api/carts.py
+++ src/api/carts.py
@@ -1,8 +1,38 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Request, HTTPException, status
 from pydantic import BaseModel
 from src.api import auth
 from enum import Enum
+import sqlalchemy
+from sqlalchemy import Integer, Column, BigInteger, String, MetaData, ForeignKey
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import relationship
+from src import database as db
 
+class Base(DeclarativeBase):
+    pass
+
+# declare GLOBAL constant table for customer_carts
+meta_customer = MetaData()
+meta_potion_cart = MetaData()
+
+customer_carts = sqlalchemy.Table("customers", 
+                    meta_customer,
+                    Column("id", Integer ,primary_key=True),
+                    Column("customer_name", String()),
+                    Column("character_class", String()),
+                    Column("level", Integer))
+
+
+class PotionCarts(Base):
+    __tablename__ = "potion_carts"
+    id = Column("id", Integer, primary_key=True)
+    customer_id = Column("customer_id", ForeignKey("customers.id"))
+    potion_sku = Column(String)
+    quantity = Column(Integer)
+
+
+
+memory_cart = []
 router = APIRouter(
     prefix="/carts",
     tags=["cart"],
@@ -72,6 +102,8 @@ class Customer(BaseModel):
     character_class: str
     level: int
 
+
+
 @router.post("/visits/{visit_id}")
 def post_visits(visit_id: int, customers: list[Customer]):
     """
@@ -85,7 +117,22 @@ def post_visits(visit_id: int, customers: list[Customer]):
 @router.post("/")
 def create_cart(new_cart: Customer):
     """ """
-    return {"cart_id": 1}
+    # create a cart for a user
+    with db.engine.begin() as connection:
+        # insert into customer_table
+        values = {"customer_name": new_cart.customer_name, 
+                "character_class": new_cart.character_class, 
+                "level": new_cart.level}
+        smtp = customer_carts.insert().values(values)
+        res = connection.execute(smtp)
+        # memory_cart.append({
+        #     "customer": new_cart,
+        #     "cart": []
+        # })
+        # memory_cart[new_cart.__hash__()]
+    # return {"cart_id": len(memory_cart) - 1}
+
+    return {"cart_id": res.inserted_primary_key[0]}
 
 
 class CartItem(BaseModel):
@@ -96,6 +143,25 @@ class CartItem(BaseModel):
 def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
     """ """
 
+
+    values = {
+        "customer_id": cart_id,
+        "sku": item_sku,
+        "quantity": cart_item.quantity
+    }
+
+    with db.engine.begin() as connection:
+        # will this be an update or insertion????
+        # assuming insertions
+        try:
+            connection.execute(sqlalchemy.text( "INSERT INTO potion_carts (customer_id, potion_sku, quantity)" +\
+                        "VALUES (:customer_id, :sku, :quantity)" 
+                        ), values)
+        except Exception as e:
+            # TODO: find out if custom messages is better, in this case it probably won't be
+            #       as invalid customer id isn't the only reason why this api would fail
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Invalid customer id.")
+      
     return "OK"
 
 
@@ -105,5 +171,21 @@ class CartCheckout(BaseModel):
 @router.post("/{cart_id}/checkout")
 def checkout(cart_id: int, cart_checkout: CartCheckout):
     """ """
+    # update db to reflect potions purchased and gold gained
+    #TODO: refactor to new potions table
+    values = {
+        "cart_id": cart_id
+    }
+    with db.engine.begin() as connection:
+        # retrieve all potions in cart
+        res = connection.execute(sqlalchemy.text(
+            "SELECT * FROM potion_carts " +\
+            "WHERE customer_id = :cart_id"
+        ), values)
+        print(res.all())
+        # update potion via sku
+        # connection.execute(sqlalchemy.text(
+        #     "UPDATE potions_cart SET "
+        # ))
 
     return {"total_potions_bought": 1, "total_gold_paid": 50}
