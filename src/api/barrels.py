72120774d55b89e25f63891d74bd51f533262a0c
Xavier Garcia <xmangarcia17@gmail.com>
2024-04-15 09:45:51 -0700

    feat: accomodate barrel delivery for red, blue ml

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -21,13 +21,21 @@ class Barrel(BaseModel):
 
 @router.post("/deliver/{order_id}") # update inventory based on order to get ingredients (ml)
 def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
+    price_of_delivery = num_green_ml = num_red_ml = num_blue_ml = 0
 
     for barrel in barrels_delivered:
-        ml_of_potion_delivery = barrel.ml_per_barrel * barrel.quantity
-        price_of_delivery = barrel.quantity * barrel.price
+        if barrel.sku == "SMALL_GREEN_BARREL":
+            num_green_ml = barrel.ml_per_barrel * barrel.quantity
+            price_of_delivery += barrel.quantity * barrel.price
+        elif barrel.sku == "SMALL_RED_BARREL":
+            num_red_ml = barrel.ml_per_barrel * barrel.quantity
+            price_of_delivery += barrel.quantity * barrel.price
+        elif barrel.sku == "SMALL_BLUE_BARREL":
+            num_blue_ml = barrel.ml_per_barrel * barrel.quantity
+            price_of_delivery += barrel.quantity * barrel.price
 
-        with db.engine.begin() as connection:
-            result = connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_green_ml = num_green_ml + {ml_of_potion_delivery}, gold = gold - {price_of_delivery}"))
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text(f"UPDATE global_inventory SET num_green_ml = num_green_ml + {num_green_ml}, num_red_ml = num_red_ml + {num_red_ml}, num_blue_ml = num_blue_ml + {num_blue_ml}, gold = gold - {price_of_delivery}"))
 
     print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
     return "OK"
@@ -36,7 +44,6 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
 # Gets called once a day
 @router.post("/plan")   # get inventory state to plan purchase of ingredients (ml)
 def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
-
     res = []
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text("SELECT num_green_potions, num_red_potions, num_blue_potions, gold FROM global_inventory"))
@@ -81,15 +88,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
                 gold -= sale.price
 
     print("Barrel Catalog: ", wholesale_catalog)
-
     print("Barrel Purchase Plan:", res)
-
-    # return [
-    #     {
-    #         "sku": "SMALL_RED_BARREL",
-    #         "quantity": 1,
-    #     }
-    # ]
 
     return res
 
