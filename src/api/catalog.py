465e4fbfb34dc76fd90e0eea704aa599b5342a54
Xavier Garcia <xmangarcia17@gmail.com>
2024-04-14 22:28:29 -0700

    feat: complete catalog for green, red, blue

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -9,36 +9,44 @@ def get_catalog():
     """
     Each unique item combination must have only a single price.
     """
+    sql_text = "SELECT num_green_potions, num_red_potions, num_blue_potions FROM global_inventory"
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("SELECT num_green_potions, num_green_ml FROM global_inventory"))
-    row1 = result.fetchone()
+        result = connection.execute(sqlalchemy.text(sql_text))
+    row = result.fetchone()
 
-    num = 0
-    if row1[0] > 0:
-        num = 1
+    # initialize catalog
+    catalog = []
 
-    if num == 0:
-        return []
-
-    # return [
-    #         {
-    #             "sku": "RED_POTION_0",
-    #             "name": "red potion",
-    #             "quantity": 1,
-    #             "price": 50,
-    #             "potion_type": [100, 0, 0, 0],
-    #         }
-    #     ]
-    catalog = [
-            {
+    # check green inventory
+    if row[0] > 0:
+        catalog.append({
                 "sku": "GREEN_POTION_0",
                 "name": "green potion",
-                "quantity": num,
-                "price": 30,
+                "quantity": 1,
+                "price": 35,
                 "potion_type": [0, 100, 0, 0]
-            }
-        ]
+            })
+        
+    # check red inventory
+    if row[1] > 0:
+        catalog.append({
+                "sku": "RED_POTION_0",
+                "name": "red potion",
+                "quantity": 1,
+                "price": 40,
+                "potion_type": [100, 0, 0, 0]
+            })
+    
+    # check blue inventory
+    if row[2] > 0:
+        catalog.append({
+                "sku": "BLUE_POTION_0",
+                "name": "blue potion",
+                "quantity": 1,
+                "price": 30,
+                "potion_type": [0, 0, 100, 0]
+            })
     
     print(f"catalog: {catalog}")
-    
+
     return catalog
