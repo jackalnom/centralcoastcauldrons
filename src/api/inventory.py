017d0c107b1faea6f81daa441295cd9824485cc4
liamhyde02 <73969644+liamhyde02@users.noreply.github.com>
2024-04-17 17:44:29 -0700

    more prints

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -70,6 +70,7 @@ def deliver_capacity_plan(capacity_purchase : CapacityPurchase, order_id: int):
     Start with 1 capacity for 50 potions and 1 capacity for 10000 ml of potion. Each additional 
     capacity unit costs 1000 gold.
     """
+    print(f"order_id: {order_id} potion_capacity: {capacity_purchase.potion_capacity} ml_capacity: {capacity_purchase.ml_capacity}")
     current_capacity_sql = "SELECT * FROM global_plan"
 
     with db.engine.begin() as connection:   
