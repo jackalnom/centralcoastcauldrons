b462b473796df5d90f1f74d6561f7247f8df59b3
Xavier Garcia <xmangarcia17@gmail.com>
2024-04-22 17:09:51 -0700

    feat: update reset to match global_inventory (not finished)

--- src/api/admin.py
+++ src/api/admin.py
@@ -16,9 +16,15 @@ def reset():
     Reset the game state. Gold goes to 100, all potions are removed from
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
-    sql_text = "UPDATE global_inventory SET num_green_potions = 0, num_red_potions = 0, num_blue_potions = 0, num_green_ml = 0, num_red_ml = 0, num_blue_ml = 0, gold = 100"
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text(sql_text))
+        result = connection.execute(sqlalchemy.text("""UPDATE global_inventory 
+                                                    SET num_red_ml = 0,
+                                                    num_green_ml = 0,  
+                                                    num_blue_ml = 0, 
+                                                    num_dark_ml = 0,
+                                                    ml_capacity = 10000,
+                                                    potion_capacity = 50,
+                                                    gold = 100"""))
 
     return "OK"
 
