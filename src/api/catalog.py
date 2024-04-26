900404cc3b2ad578789b744f5f661ad5b0865b18
Jesus Avalos <jesusavalosr2004@gmail.com>
2024-04-25 19:22:08 -0700

    added custom potions to catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -19,34 +19,25 @@ def get_catalog():
     with db.engine.begin() as connection:
         # result = connection.execute(sqlalchemy.text(f"SELECT * FROM global_inventory WHERE id = 2"))
         # get green potion prop.
-        result = connection.execute(sqlalchemy.text("SELECT * FROM potions "))
-        try:
-            inventory = result.mappings().all()
-        except Exception as e:
-            print(e)
-            return []
+        result = connection.execute(sqlalchemy.text("SELECT potions.potion_sku, potions.name, potions.price, " +\
+                                                    "potions.red, potions.green, potions.blue, " +\
+                                                    "potions.dark, COALESCE(SUM(ledger.change), 0) AS quantity " +\
+                                                    "FROM potions " +\
+                                                    "LEFT JOIN potion_ledger AS ledger ON " +\
+                                                    "potions.potion_sku = ledger.potion_sku " +\
+                                                    "GROUP BY potions.potion_sku"))
 
-    # parse quantity 
-    '''
-    Custom Potions??? Need more documentation on format of potions.
-
-    potion_re = re.compile(r"num_(\w*)_potions")
-    color = None 
-    # iterate through keys of db and find matches to add to SKU
-    for key in result.keys():
-        if match := potion_re.match(key):
-            color = match.group(1)
-    '''
-    # iterate and add to catalog if the name of the given potion color matches the regex
-    for potion in inventory:
-        #TODO: allow functionality to purchase more than 1 potions
-        if potion["quantity"] <= 0:
-            continue
-        catalog.append({
-            "sku": potion["potion_sku"],
-            "name": potion["name"],
-            "quantity": potion["quantity"],
-            "price": potion["price"],
-            "potion_type": [potion["red"], potion["green"], potion["blue"], potion["dark"]],
-        })
-    return catalog
+        # iterate and add to catalog if the name of the given potion color matches the regex
+        # FORMAT: sku, name, price, r, g, b, d, quantity
+        for potion in result:
+            #TODO: allow functionality to purchase more than 1 potions
+            if potion[7] <= 0:
+                continue
+            catalog.append({
+                "sku": potion[0],
+                "name": potion[1],
+                "quantity": potion[7],
+                "price": potion[2],
+                "potion_type": [potion[3], potion[4], potion[5], potion[6]],
+            })
+        return catalog
