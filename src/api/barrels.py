9313c0a5b1efdf60d1790bb6b83d8002a3aa03f0
Xavier Garcia <xmangarcia17@gmail.com>
2024-04-27 00:09:36 -0700

    complete wholesale purchase plan logic

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -63,8 +63,6 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     print("CALLED get_wholesale_purchase_plan()")
     print("Barrel Catalog: ", wholesale_catalog)
 
-    wholesale_catalog.sort(key=lambda x: x.sku)
-    res = []
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text("SELECT * FROM global_inventory"))
     row = result.fetchone()
@@ -76,63 +74,120 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
 
     ml_room = row.ml_capacity - (num_red + num_green + num_blue + num_dark)
 
-    # bootstrap = False
-    # if row.gold < 400:
-    #     gold = row.gold
-    #     bootstrap = True
-    # else:
-    #     gold = row.gold // 4
+    red_memory = []
+    green_memory = []
+    blue_memory = []
+    dark_memory = []
 
-
+    # unpack what is in the catalog
     for sale in wholesale_catalog:
-        quantity = 0
-        # sale.sku == "SMALL_RED_BARREL"
-        if sale.potion_type == [1, 0, 0, 0] and sale.ml_per_barrel == 500:
-            if num_red < 100 and gold >= sale.price and ml_room >= sale.ml_per_barrel:
-                quantity = 1
-            if quantity > 0:
-                res.append({
-                    "sku": sale.sku,
-                    "quantity": quantity
-                })
-                gold -= sale.price
-                ml_room -= quantity * sale.ml_per_barrel
-        # sale.sku == "SMALL_GREEN_BARREL"
-        elif sale.potion_type == [0, 1, 0, 0] and sale.ml_per_barrel == 500:
-            if num_green < 100 and gold >= sale.price and ml_room >= sale.ml_per_barrel:
-                quantity = 1 
-            if quantity > 0:
-                res.append({
-                    "sku": sale.sku,
-                    "quantity": quantity
-                })
-                gold -= sale.price
-                ml_room -= quantity * sale.ml_per_barrel
-        # sale.sku == "SMALL_BLUE_BARREL"
-        elif sale.potion_type == [0, 0, 1, 0] and sale.ml_per_barrel == 500:
-            if num_blue < 100 and gold >= sale.price and ml_room >= sale.ml_per_barrel:
-                quantity = 1
-            if quantity > 0:
-                res.append({
-                    "sku": sale.sku,
-                    "quantity": quantity
-                }) 
-                gold -= sale.price
-                ml_room -= quantity * sale.ml_per_barrel
-        # sale.sku == "LARGE_DARK_BARREL"
+        if sale.potion_type == [1, 0, 0, 0]:
+            red_memory.append(sale)
+        elif sale.potion_type == [0, 1, 0, 0]:
+            green_memory.append(sale)
+        elif sale.potion_type == [0, 0, 1, 0]:
+            blue_memory.append(sale)
         elif sale.potion_type == [0, 0, 0, 1]:
-            if gold >= sale.price and ml_room >= sale.ml_per_barrel:
-                quantity = 1
-            if quantity > 0:
-                res.append({
-                    "sku": sale.sku,
-                    "quantity": quantity
-                })
-                gold -= sale.price
-                ml_room -= quantity * sale.ml_per_barrel
+            dark_memory.append(sale)
 
-    print("Sorted Barrel Catalog: ", wholesale_catalog)
-    print("Barrel Purchase Plan:", res)
+    dark_in_cat = True
+    if row.gold > 2000:
+        budget = (gold * 7) // 10   # budget = 70% of gold
+        if dark_memory == []:   # if there are no dark barrels in catalog
+            dark_in_cat = False
+            budget = budget // 3
+        budget = budget // 4        # can spend 25% of budget at a time
+    else:
+        budget = gold
+    
+    red_bud = green_bud = blue_bud = dark_bud = budget  # all colors have certain budget
 
-    return res
+    if not dark_in_cat:
+        dark_bud = 0
+
+    red_memory.sort(key=lambda x: x.price, reverse=True)
+    green_memory.sort(key=lambda x: x.price, reverse=True)
+    blue_memory.sort(key=lambda x: x.price, reverse=True)
+    dark_memory.sort(key=lambda x: x.price, reverse=True)
+
+    # create wishlist
+    # barrel in memory: (sku, price)
+    cont = True
+    temp_barrel_wishlist = {}
+    while (red_bud > 0 or green_bud > 0 or blue_bud > 0 or dark_bud > 0) and ml_room > 0 and cont:
+        cont = False
+        # select a red barrel
+        for i in range(len(red_memory)):
+            red_bar = red_memory[i]
+            if red_bar.price <= red_bud and red_bar.ml_per_barrel <= ml_room and budget >= red_bar.price:
+                if red_bar.sku in temp_barrel_wishlist:
+                    temp_barrel_wishlist[red_bar.sku] += 1
+                else:
+                    temp_barrel_wishlist[red_bar.sku] = 1
+                red_bud -= red_bar.price
+                budget -= red_bar.price     # precautionary step
+                ml_room -= red_bar.ml_per_barrel
+                red_memory[i].quantity -= 1
+                cont = True
+                break
+
+        # select a green barrel
+        for i in range(len(green_memory)):
+            green_bar = green_memory[i]
+            if green_bar.price <= green_bud and green_bar.ml_per_barrel <= ml_room and budget >= red_bar.price:
+                if green_bar.sku in temp_barrel_wishlist:
+                    temp_barrel_wishlist[green_bar.sku] += 1
+                else:
+                    temp_barrel_wishlist[green_bar.sku] = 1
+                green_bud -= green_bar.price
+                budget -= green_bar.price     # precautionary step
+                ml_room -= green_bar.ml_per_barrel
+                green_memory[i].quantity -= 1
+                cont = True
+                break
+
+        # select a blue barrel
+        for i in range(len(blue_memory)):
+            blue_bar = blue_memory[i]
+            if blue_bar.price <= blue_bud and blue_bar.ml_per_barrel <= ml_room and budget >= red_bar.price:
+                if blue_bar.sku in temp_barrel_wishlist:
+                    temp_barrel_wishlist[blue_bar.sku] += 1
+                else:
+                    temp_barrel_wishlist[blue_bar.sku] = 1
+                blue_bud -= blue_bar.price
+                budget -= blue_bar.price     # precautionary step
+                ml_room -= blue_bar.ml_per_barrel
+                blue_memory[i].quantity -= 1
+                cont = True
+                break
+
+        # select a dark barrel
+        for i in range(len(dark_memory)):
+            dark_bar = dark_memory[i]
+            if dark_bar.price <= dark_bud and dark_bar.ml_per_barrel <= ml_room and budget >= red_bar.price:
+                if dark_bar.sku in temp_barrel_wishlist:
+                    temp_barrel_wishlist[dark_bar.sku] += 1
+                else:
+                    temp_barrel_wishlist[dark_bar.sku] = 1
+                dark_bud -= dark_bar.price
+                budget -= dark_bar.price     # precautionary step
+                ml_room -= dark_bar.ml_per_barrel
+                dark_memory[i].quantity -= 1
+                cont = True
+                break
+    
+    keys, values = temp_barrel_wishlist.keys(), temp_barrel_wishlist.values()
+    keys = list(keys)
+    values = list(values)
+    purchase_plan = []
+    for i in range(len(values)):
+        if values[i] > 0:
+            purchase_plan.append({
+                "sku": keys[i],
+                "quantity": values[i]
+            })
+
+    print("Barrel Purchase Plan:", purchase_plan)
+
+    return purchase_plan
 
