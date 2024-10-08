39c9b59eeca698c883ec87e1ed54f4576a95e9af
coryfcowden <cfcowden@calpoly.edu>
2024-10-07 23:49:23 -0700

    red and blue for catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -14,29 +14,61 @@ def get_catalog():
 
     with db.engine.begin() as connection:
 
-        sqlpotionamt = "SELECT num_green_potions FROM global_inventory"
-        sqlmlamt = "SELECT num_green_ml FROM global_inventory"
+        
+        sqlpotionamt = "SELECT num_red_potions, num_green_potions, num_blue_potions FROM global_inventory"
+        sqlmlamt = "SELECT num_red_ml, num_green_ml, num_blue_ml FROM global_inventory"
 
-        potionamt = connection.execute(sqlalchemy.text(sqlpotionamt)).scalar()
-        mlamt = connection.execute(sqlalchemy.text(sqlmlamt)).scalar()
+        potionamt = connection.execute(sqlalchemy.text(sqlpotionamt))
+        mlamt = connection.execute(sqlalchemy.text(sqlmlamt))
 
-        if(potionamt >= 1 and mlamt >= 100): 
-            return [
-            {
+        red_potions = potionamt[0]
+        green_potions = potionamt[1]
+        blue_potions = potionamt[2]
+
+        red_ml = mlamt[0]
+        green_ml = mlamt[1]
+        blue_ml = mlamt[2]
+
+        #catalog list to be returned
+        catalog = []
+
+        #if we have at least one red potion, add 1 red potion to catalog
+        if red_potions >= 1 and red_ml >= 100:
+            catalog.append({
+                "sku": "RED_POTION_0",
+                "name": "red potion",
+                "quantity": 1,
+                "price": 100,
+                "potion_type": [1, 0, 0, 0],
+            })
+
+        #if we have at least one green potion, add 1 green potion to catalog
+        if green_potions >= 1 and green_ml >= 100:
+            catalog.append({
                 "sku": "GREEN_POTION_0",
                 "name": "green potion",
                 "quantity": 1,
                 "price": 100,
-                "potion_type": [0, 100, 0, 0],
-            }
+                "potion_type": [0, 1, 0, 0],
+            })
+
+        #if we have at least one blue potion, add 1 blue potion to catalog
+        if blue_potions >= 1 and blue_ml >= 100:
+            catalog.append({
+                "sku": "BLUE_POTION_0",
+                "name": "blue potion",
+                "quantity": 1,
+                "price": 100,
+                "potion_type": [0, 0, 1, 0],
+            })
+
+    if not catalog:
+        return [
+                {
+                    "sku": "",
+                    "name": "",
+                    "quantity": 0,
+                    "price": 0,
+                    "potion_type": [0, 0, 0, 0],
+                }
             ]
-        
-    return [
-            {
-                "sku": "",
-                "name": "",
-                "quantity": 0,
-                "price": 0,
-                "potion_type": [0, 0, 0, 0],
-            }
-        ]
